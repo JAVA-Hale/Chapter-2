d746427be4f2e26682261f7e89b2b44ccd87ef85
Thomas hale <TH146688@davenportschools.org>
2019-09-09 08:29:07 -0500

    update change

---
+++ src/Eggs.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Eggs {
+    public static void main(String[] args){
+
+        double dozenEggs = 3.25;
+        double singleEgg = .45;
+        double numEggs;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number of eggs you want to buy >>> ");
+        numEggs = keyboard.nextInt();
+
+        numEggs = dozenEggs + singleEgg;
+
+        System.out.println("You ordered " + numEggs + "."
+    }
+}
